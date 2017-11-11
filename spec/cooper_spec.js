f867eaf95d3cf956a6e52ae4a7b45197c89cb062
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-11 18:45:46 +0100

    Change approach, rewrite specs and code

--- spec/cooper_spec.js
+++ spec/cooper_spec.js
@@ -1,17 +1,31 @@
-describe("person", function() {
-  it('returns "Poor" if distance is less than 2100 meters', function(){
-    expect(person({age: 13, gender: 'female', distance: 2000})).toEqual('Poor');
-  });
-  it('returns "Below Average" if distance is less than 2200 meters', function(){
-    expect(person({age: 14, gender: 'female', distance: 2100})).toEqual('Below Average');
-  });
-  it('returns "Average" if distance is less than 2400 meters', function(){
-    expect(person({age: 13, gender: 'female', distance: 2200})).toEqual('Average');
-  });
-  it('returns "Above Average" if distance is less than 2700 meters', function(){
-    expect(person({age: 13, gender: 'female', distance: 2400})).toEqual('Above Average');
-  });
-  it('returns "Excellent" if distance is greater than 2700 meters', function(){
-    expect(person({age: 13, gender: 'female', distance: 2700})).toEqual('Excellent');
-  });
-});
+describe("Person", function() {
+  var person;
+
+  beforeEach(function() {
+    person = new Person({gender: 'female', age: 13, distance: 2000});
+  })
+  it('gets an assessment based on gender', function() {
+    expect(person.gender).toEqual('female');
+  })
+  it('gets an assessment based on age', function() {
+    expect(person.age).toEqual(13);
+  })
+  it('gets an assessment based on completed distance', function() {
+    expect(person.distance).toEqual(2000);
+  })
+  it('gets an assessment description "Poor" if distance is less than 2100 meters', function(){
+    expect(person.description).toEqual('Poor');
+  })
+  // it('returns "Below Average" if distance is less than 2200 meters', function(){
+  //   expect(person.description).toEqual('Below Average');
+  // });
+  // it('returns "Average" if distance is less than 2400 meters', function(){
+  //   expect(person.description).toEqual('Average');
+  // });
+  // it('returns "Above Average" if distance is less than 2700 meters', function(){
+  //   expect(person.description).toEqual('Above Average');
+  // });
+  // it('returns "Excellent" if distance is greater than 2700 meters', function(){
+  //   expect(person.description).toEqual('Excellent');
+  // });
+})
